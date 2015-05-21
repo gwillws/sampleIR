3a5d00004bc39a7a07b8137a0d361c76b627194a
jones <jones.isican@gmail.com>
2015-05-21 17:59:57 +0800

    commit message

---
+++ sampleIR/client/router.js
@@ -0,0 +1,8 @@
+Router.configure({
+	layoutTemplate: 'layout'
+});
+Router.map(function(){
+	this.route('home',{path:'/'})
+	this.route('about',{path:'/about'})
+	this.route('builder',{path:'/builder'})
+})
